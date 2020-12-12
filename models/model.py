38d5422de57b77135527ba423f64118add93b08d
Omer Ahmed - home <omar@sonod.tech>
2020-12-12 19:51:05 +0300

    add some new requiremint

--- models/model.py
+++ models/model.py
@@ -10,7 +10,19 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     default_uom_id = fields.Many2one(
-        'uom.uom', 'Default Sale Unit of Measure')
+        'uom.uom', 'Default Unit of Measure')
+    
+    default_uom_qty_available = fields.Float(
+        compute='_compute_default_uom_qty_available', string='Quantity Available')
+    
+    @api.depends('default_uom_id', 'uom_id')
+    def _compute_default_uom_qty_available(self):
+        for rec in self:
+            rec.default_uom_qty_available = 0
+            if rec.uom_id and rec.default_uom_id:
+                rec.default_uom_qty_available = rec.uom_id._compute_quantity(
+                    rec.qty_available, rec.default_uom_id)
+
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
@@ -79,5 +91,35 @@ class PurchaseOrderLine(models.Model):
         if res.get('domain') and self.product_id:
             if res['domain'].get('product_uom'):
                 res['domain']['product_uom'] = [
+                    ('id', 'in', self.product_id.get_domain_ids())]
+        return res
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        res = super(StockMove, self).onchange_product_id()
+        if self.product_id:
+            self.product_uom = self.product_id.get_sale_default_uom_id()
+        if res.get('domain') and self.product_id:
+            if res['domain'].get('product_uom'):
+                res['domain']['product_uom'] = [
+                    ('id', 'in', self.product_id.get_domain_ids())]
+        return res
+
+
+class StockInventoryLine(models.Model):
+    _inherit = 'stock.inventory.line'
+
+    @api.onchange('product_id')
+    def _onchange_product(self):
+        res = super(StockInventoryLine, self).onchange_product_id()
+        if self.product_id:
+            self.product_uom_id = self.product_id.get_sale_default_uom_id()
+        if res.get('domain') and self.product_id:
+            if res['domain'].get('product_uom_id'):
+                res['domain']['product_uom_id'] = [
                     ('id', 'in', self.product_id.get_domain_ids())]
         return res
